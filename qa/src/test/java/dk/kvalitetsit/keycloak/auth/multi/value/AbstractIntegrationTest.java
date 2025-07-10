f161156271b415b7143faecd5fd48cf180aa671c
Amalie Dobel Christiansen <adc@kvalitetsit.dk>
2025-07-10 10:02:23 +0200

    Update tests to support Keycloak 26.

--- qa/src/test/java/dk/kvalitetsit/keycloak/auth/multi/value/AbstractIntegrationTest.java
+++ qa/src/test/java/dk/kvalitetsit/keycloak/auth/multi/value/AbstractIntegrationTest.java
@@ -4,11 +4,13 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebClientOptions;
 import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
 import dk.kvalitetsit.keycloak.auth.multi.value.dto.Credential;
 import dk.kvalitetsit.keycloak.auth.multi.value.dto.User;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.BeforeClass;
@@ -26,9 +28,8 @@ import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.*;
 import java.util.function.Consumer;
@@ -37,7 +38,7 @@ public class AbstractIntegrationTest {
     private static final Logger logger = LoggerFactory.getLogger(AbstractIntegrationTest.class);
     private static final Logger keycloakLogger = LoggerFactory.getLogger("keycloak-logger");
 
-    public static Integer keycloakPort;
+    public static int keycloakPort;
     private static String keycloakHost;
 
     private static final RestTemplate restTemplate = new RestTemplate();
@@ -47,22 +48,18 @@ public class AbstractIntegrationTest {
     private static String proxyHost;
 
     @BeforeClass
-    public static void setupTestEnvironment() throws JSONException, IOException {
+    public static void setupTestEnvironment() throws JSONException, IOException, XmlPullParserException {
         Network n = Network.newNetwork();
 
         // Start Keycloak service
         var version = calculateKeycloakVersion();
-        GenericContainer<?> keycloakContainer = getKeycloakContainer(n, version);
-        keycloakContainer.start();
+        setupKeycloakContainer(n, version);
         setupProxyContainer(n);
 
-        logContainerOutput(keycloakContainer, keycloakLogger);
-        keycloakPort = keycloakContainer.getMappedPort(8080);
-        keycloakHost = keycloakContainer.getHost();
         accessToken = getAccessToken();
     }
 
-    private static GenericContainer<?> getKeycloakContainer(Network n, String tag) {
+    private static void setupKeycloakContainer(Network n, String tag) {
         logger.info("Starting keycloak container version {}", tag);
         Consumer<CreateContainerCmd> cmd = e -> e.withPortBindings(new PortBinding(Ports.Binding.bindPort(8080), new ExposedPort(8080)));
 
@@ -73,12 +70,12 @@ public class AbstractIntegrationTest {
                                 .copy("keycloak-auth-multi-value.jar", "/opt/keycloak/providers/keycloak-auth-multi-value.jar")
                                 .build());
 
-        return new GenericContainer<>(image)
+        var keycloakContainer = new GenericContainer<>(image)
                 .withClasspathResourceMapping("test-realm.json", "/opt/keycloak/data/import/test-realm.json", BindMode.READ_ONLY)
                 .withClasspathResourceMapping("attribute-realm.json", "/opt/keycloak/data/import/attribute-realm.json", BindMode.READ_ONLY)
                 .withCommand("start-dev", "--http-relative-path", "/auth", "--import-realm")
-                .withEnv("KEYCLOAK_ADMIN", "kit")
-                .withEnv("KEYCLOAK_ADMIN_PASSWORD", "Test1234")
+                .withEnv("KC_BOOTSTRAP_ADMIN_USERNAME", "kit")
+                .withEnv("KC_BOOTSTRAP_ADMIN_PASSWORD", "Test1234")
                 .withEnv("KEYCLOAK_LOGLEVEL", "DEBUG")
                 .withEnv("required_action_choose_attribute_attribute_name", "organisation")
 
@@ -87,24 +84,19 @@ public class AbstractIntegrationTest {
                 .withExposedPorts(8080)
                 .withCreateContainerCmdModifier(cmd)
                 .waitingFor(Wait.forHttp("/auth").withStartupTimeout(Duration.ofMinutes(3)));
+
+        keycloakContainer.start();
+
+        logContainerOutput(keycloakContainer, keycloakLogger);
+        keycloakPort = keycloakContainer.getMappedPort(8080);
+        keycloakHost = keycloakContainer.getHost();
     }
 
-    private static String calculateKeycloakVersion() throws IOException {
-        var properties = new Properties();
-        var path = Paths.get(".").toAbsolutePath().normalize().toString();
-        if(path.endsWith("qa")) {
-            properties.load(new FileInputStream("../target/classes/git.properties"));
-        }
-        else {
-            properties.load(new FileInputStream("target/classes/git.properties"));
-        }
-        var gitBranch = properties.getProperty("git.branch");
-
-        if(gitBranch.startsWith("keycloak-")) {
-            return gitBranch.substring(9);
-        }
-
-        return "latest";
+    private static String calculateKeycloakVersion() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("../pom.xml"));
+        var properties = model.getProperties();
+        return properties.getProperty("keycloak.version");
     }
 
     private static void setupProxyContainer(Network n) {
@@ -143,17 +135,16 @@ public class AbstractIntegrationTest {
                 .waitingFor(Wait.forListeningPort());
         oauthProxy.start();
         proxyPort = oauthProxy.getMappedPort(4180);
-        proxyHost = oauthProxy.getContainerIpAddress();
+        proxyHost = oauthProxy.getHost();
     }
 
     private static void logContainerOutput(GenericContainer<?> container, Logger logger) {
-        logger.info("Attaching logger to container: " + container.getContainerInfo().getName());
+        logger.info("Attaching logger to container: {}", container.getContainerInfo().getName());
         Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
         container.followOutput(logConsumer);
     }
 
     public WebClient getWebDriver() {
-
         WebClient wc = new WebClient();
         WebClientOptions options = wc.getOptions();
         options.setJavaScriptEnabled(true);
@@ -178,7 +169,7 @@ public class AbstractIntegrationTest {
         user.setEmail(String.format("%s@example.com", UUID.randomUUID()));
         user.getCredentials().add(credential);
         user.getAttributes().put("USER_ID", Collections.singletonList(userId));
-        if (attributeList.size() > 0) {
+        if (!attributeList.isEmpty()) {
             // key skal stemme overens med required_action_choose_attribute_attribute_name
             user.getAttributes().put("organisation", attributeList);
         }
@@ -254,10 +245,10 @@ public class AbstractIntegrationTest {
     }
 
     public static String appendToKeycloakHostAndPort(String url) {
-        return "http://"+ keycloakHost +":"+keycloakPort+url;
+        return "http://" + keycloakHost + ":" + keycloakPort + url;
     }
 
     public static String appendToProxyHostAndPort(String url) {
-        return "http://" + proxyHost + ":" + proxyPort+url;
+        return "http://" + proxyHost + ":" + proxyPort + url;
     }
 }
